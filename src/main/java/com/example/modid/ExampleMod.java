19edd272256cb1dbabd673f5a1d06ebef152b8ef
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-15 01:03:38 +0800

    Update ExampleMod.java

--- src/main/java/com/example/modid/ExampleMod.java
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -8,17 +8,26 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = ExampleMod.MOD_ID, name = ExampleMod.MOD_NAME, version = ExampleMod.VERSION)
 public class ExampleMod {
 
-    public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_NAME);
+    public static final String MOD_ID = "modid";
+    public static final String MOD_NAME = "Mod Name";
+    public static final String VERSION = "1.0.0";
 
-    @SidedProxy(modId = Reference.MOD_ID, clientSide = "com.example.modid.proxy.ClientProxy", serverSide = "com.example.modid.proxy.CommonProxy")
+    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+
+    @SidedProxy(modId = MOD_ID, clientSide = "com.example.modid.proxy.ClientProxy", serverSide = "com.example.modid.proxy.CommonProxy")
     public static IProxy proxy;
-    
+
+    /**
+     * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
+     *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
+     * </a>
+     */
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Hello From {}!", Reference.MOD_NAME);
+        LOGGER.info("Hello From {}!", MOD_NAME);
         LOGGER.info("Proxy is {}", proxy);
         LOGGER.info("Language: {}", Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage());
     }
